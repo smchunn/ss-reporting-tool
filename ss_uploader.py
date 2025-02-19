67631cf34dec437e74f40b78eca982c9ffb138af
smchunn <smchunn@icloud.com>
2025-02-19 14:54:10 -0600

    Feature: change to config class

--- ss_uploader.py
+++ ss_uploader.py
@@ -1,11 +1,17 @@
+from os.path import isfile
 import sys, os, logging
-import toml, openpyxl
+import toml, json
 from datetime import datetime
 import ss_api
 import concurrent.futures, threading
+import polars as pl
+from polars import col, lit
+from datetime import datetime, timezone
+from typing import List, Dict, Callable, Union
 
 
 start_time = datetime.now()
+
 
 class TomlLineBreakPreservingEncoder(toml.TomlEncoder):
     def __init__(self, _dict=dict, preserve=False):
@@ -30,6 +36,7 @@ class Config:
         import argparse
 
         argparser = argparse.ArgumentParser(add_help=True)
+        argparser.add_argument("func", type=str, help="function to run: ")
         argparser.add_argument(
             "-c",
             "--config",
@@ -38,8 +45,10 @@ class Config:
             default="./config.toml",
         )
         argparser.add_argument("--verbose", action="store_true")
-        argparser.add_argument( "--threadcount", help="set # of threads", default=None)
+        argparser.add_argument("--threadcount", help="set # of threads", default=None)
         args = argparser.parse_args()
+
+        self.function = args.func
 
         self.path = os.path.abspath(os.path.expanduser(args.config))
 
@@ -53,29 +62,33 @@ class Config:
                 os.environ[k] = v
             if self._config.get("verbose", False):
                 self.verbose = True
-            self.root_id = self._config.get("root", None).get("folder_id", None)
-            self.root_dir = self._config.get("root", None).get("dir", "./")
+            self.data_dir = self._config.get("data_dir", "./data")
+            if not os.path.exists(self.data_dir):
+                os.mkdir(self.data_dir)
+            elif os.path.isfile(self.data_dir):
+                print(f"Error: data dir exists as file")
+                exit()
 
-            for k, v in self._config.get("root", None).items():
-                # if child is a folder
-                if "folder_id" in v:
-                    pass
-                # otherwise it is a sheet
-                else:
-                    table_id = v["id"]
-                    table_src = (
-                        os.path.join(self.data_dir, v["src"])
-                        if os.path.isfile(os.path.join(self.data_dir, v["src"]))
-                        else ""
+            self.target_folder = self._config.get("target_folder", None)
+            for k, v in self._config["tables"].items():
+                table_id = v["id"]
+                table_src = (
+                    os.path.join(self.data_dir, v["src"])
+                    if os.path.isfile(os.path.join(self.data_dir, v["src"]))
+                    else ""
+                )
+                table_name = k
+                table_refresh = v.get("date", datetime.now())
+                self.tables.append(
+                    Table(
+                        table_id,
+                        self.target_folder,
+                        table_name,
+                        table_src,
+                        table_refresh,
                     )
-                    table_name = k
-                    self.tables.append(
-                        Table(table_id, parent_id, table_name, table_src)
-                    )
+                )
         self.verbose = self.verbose or args.verbose
-        self.reaudit = args.reaudit
-        self.excel_only = args.excel_only
-        self.data_dir = self.data_dir or "./"
 
         if self.verbose:
             logging.basicConfig(
@@ -96,96 +109,163 @@ class Config:
         with open(self.path, "w") as conf:
             encoder = TomlLineBreakPreservingEncoder()
             toml.dump(self._config, conf, encoder=encoder)
+
+
 class Table:
-    def __init__(self, id, parent_id, name, source):
+    config: Config
+
+    def __init__(self, id, parent_id, name, src, last_update) -> None:
+        self.name = name
         self.id = id
         self.parent_id = parent_id
-        self.name = name
-        self.source = source
+        self.src = src
+        self.last_update = last_update
+        self.data: pl.DataFrame = pl.DataFrame()
+        self.sheet_id_to_col_map = None
+        self.sheet_col_to_id_map = None
+
+    def __bool__(self) -> bool:
+        return not self.data.is_empty()
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+    def update_refresh(self) -> None:
+        self.refresh = str(datetime.now(timezone.utc).isoformat(timespec="seconds"))
+
+    def load_from_ss(self) -> None:
+        sheet_json = ss_api.get_sheet(self.id)
+        self.update_refresh()
+        if not isinstance(sheet_json, Dict):
+            return
+        with open(
+            os.path.join(Table.config.data_dir, f"{self.name}.json"), "w"
+        ) as file:
+            json.dump(sheet_json, file)
+
+        self.sheet_id_to_col_map = {
+            col["id"]: col["title"] for col in sheet_json["columns"]
+        }
+        self.sheet_col_to_id_map = {
+            col["title"]: col["id"] for col in sheet_json["columns"]
+        }
+
+        data = []
+        for row in sheet_json["rows"]:
+            row_data = {}
+            row_data["_id"] = row["id"]
+            row_data["_created"] = row["createdAt"]
+            row_data["_modified"] = row["modifiedAt"]
+            # row_data["AUDIT_BY"] = row["modifiedBy"]
+            for cell in row["cells"]:
+                column_title = self.sheet_id_to_col_map[cell["columnId"]]
+                row_data[column_title] = cell.get("value", None)
+            data.append(row_data)
+
+        self.data = pl.DataFrame(data, infer_schema_length=None)
+
+    def load_from_file(self) -> None:
+        self.data = pl.read_csv(self.src, separator=chr(31))
+
+    def export_to_excel(self) -> None:
+        excel_fp = os.path.join(Table.config.data_dir, f"{self.name}.xlsx")
+        self.data.write_excel(
+            workbook=excel_fp,
+            worksheet="audit",
+            table_name="audit_table",
+            table_style="Table Style Medium 9",
+            autofit=True,
+            freeze_panes=(1, 0),
+        )
+
+    def export_to_ss(self) -> None:
+        excel_fp = os.path.join(Table.config.data_dir, f"{self.name}.xlsx")
+        ss_target_folder = Table.config.target_folder
+        result = ss_api.import_xlsx_sheet(self.name, excel_fp, ss_target_folder)
+        if result:
+            self.id = str(result["result"]["id"])
+            self.update_refresh()
+            print(f"  {self.name}({self.id}): new table loaded")
+        Table.config.serialize()
+
+    def update_ss(self) -> None:
+        if isinstance(self.sheet_col_to_id_map, dict) and isinstance(
+            self.sheet_id_to_col_map, dict
+        ):
+            data = [
+                {
+                    "id": row["_id"],
+                    "cells": [
+                        {"columnId": self.sheet_col_to_id_map[col], "value": val}
+                        for col, val in row.items()
+                        if col
+                        in [
+                            "USER_STATUS",
+                            "AUDIT_BY",
+                            "EPICC_STATUS",
+                            "MXI_REMAINING",
+                            "TRAX_REMAINING",
+                            "_MXI_BASELINE",
+                        ]
+                    ],
+                }
+                for row in self.data.filter("_UPDATE").iter_rows(named=True)
+            ]
+            print(f"exporting {self.name}({self.id})")
+            ss_api.update_sheet(self.id, data)
+        Table.config.serialize()
+
 
 def get_sheet():
-    if isinstance(CONFIG, dict):
-        print("Starting ...")
-        if "verbose" in CONFIG and CONFIG["verbose"] == True:
-            logging.basicConfig(filename="sheet.log", level=logging.INFO)
+    print("Starting ...")
 
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            # Submit all table processing tasks to the executor
-            futures = [
-                executor.submit(get_single_sheet, table["id"], table_name)
-                for table_name, table in CONFIG["tables"].items()
-            ]
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Submit all table processing tasks to the executor
+        futures = [
+            executor.submit(get_single_sheet, table) for table in Table.config.tables
+        ]
 
-            # Collect results as they complete
-            for x, future in enumerate(concurrent.futures.as_completed(futures)):
-                pass
-                print(f"thread no. {x} returned")
-
-        # for k, v in CONFIG["tables"].items():
-        #     table_id = v["id"]
-        #     table_src = v["src"]
-        #     table_name = k
-        #     # Pass the folder_id to the get_sheet_as_excel function
-        #     ss_api.get_sheet_as_xlsx(
-        #         table_id, os.path.join(_dir_out, f"{table_name}.xlsx")
-        #     )
+        # Collect results as they complete
+        for x, _ in enumerate(concurrent.futures.as_completed(futures)):
+            pass
+            print(f"thread no. {x} returned")
 
 
-def get_single_sheet(table_id, table_name):
-    print(f"Getting {table_name} as xslx")
-    ss_api.get_sheet_as_xlsx(table_id, os.path.join(_dir_out, f"{table_name}.xlsx"))
+def get_single_sheet(table: Table):
+    print(f"Getting {table.name} as xslx")
+    ss_api.get_sheet_as_xlsx(
+        table.id, os.path.join(Table.config.path, f"{table.name}.xlsx")
+    )
 
 
 def set_sheet():
     print("Starting ...")
-    if isinstance(CONFIG, dict):
-        target_workspace_id = CONFIG.get("env", {}).get("target_workspace")
-        target_folder_id = CONFIG.get("env", {}).get("target_folder")
 
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            # Submit all table processing tasks to the executor
-            futures = [
-                executor.submit(
-                    set_single_sheet,
-                    table["id"],
-                    table_name,
-                    table["src"],
-                    target_folder_id,
-                )
-                for table_name, table in CONFIG["tables"].items()
-            ]
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Submit all table processing tasks to the executor
+        futures = [
+            executor.submit(set_single_sheet, table) for table in Table.config.tables
+        ]
 
-            # Collect results as they complete
-            for x, future in enumerate(concurrent.futures.as_completed(futures)):
-                result = future.result()
-                if isinstance(result, tuple) and result[0] == "new_id":
-                    CONFIG["tables"][result[1]]["id"] = result[2]
-                print(f"thread no. {x} returned")
-
-        # for k, v in CONFIG["tables"].items():
-        #     table_id = v["id"]
-        #     table_src = v["src"]
-        #     table_name = k
+        # Collect results as they complete
+        for x, _ in enumerate(concurrent.futures.as_completed(futures)):
+            print(f"thread no. {x} returned")
 
 
-def set_single_sheet(table_id, table_name, table_src, target_folder_id):
-    print(f"starting {table_name}...")
+def set_single_sheet(table: Table):
+    print(f"starting {table.name}...")
 
-    if not table_id:
-        print(f"No existing table, uploading {table_src} to {table_name}")
+    if not table.id:
+        print(f"No existing table, uploading {table.src} to {table.name} in folder {table.parent_id}")
         result = ss_api.import_xlsx_sheet(
-            sheet_name=table_name,
-            filepath=os.path.join(_dir_in, table_src),
-            folder_id=target_folder_id if target_folder_id else None,
+            sheet_name=table.name, filepath=table.src, folder_id=table.parent_id
         )
         if result:
-            table_id = str(result["result"]["id"])
-            print(f"  {table_name}({table_id}): new table loaded")
-            return ("new_id", table_name, table_id)
+            table.id = str(result["result"]["id"])
+            print(f"  {table.name}({table.id}): new table loaded")
     else:
         result = ss_api.import_xlsx_sheet(
-            sheet_name=f"TMP_{table_name}",
-            filepath=os.path.join(_dir_in, table_src),
+            sheet_name=f"TMP_{table.name}", filepath=table.src
         )
         if not result:
             return
@@ -195,7 +275,7 @@ def set_single_sheet(table_id, table_name, table_src, target_folder_id):
             return
 
         import_sheet_id = result["result"]["id"]
-        target_sheet_id = table_id
+        target_sheet_id = table.id
 
         if not import_sheet_id or not target_sheet_id:
             return
@@ -203,6 +283,7 @@ def set_single_sheet(table_id, table_name, table_src, target_folder_id):
         ss_api.clear_sheet(target_sheet_id)
         ss_api.move_rows(target_sheet_id, import_sheet_id)
         ss_api.delete_sheet(import_sheet_id)
+    Table.config.serialize()
     print("done...")
 
 
@@ -212,18 +293,17 @@ def update_sheet():
     and "Created Date" and "Modified Date" as date columns.
     """
     print("Updating columns ...")
-    if isinstance(CONFIG, dict):
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            # Submit all table processing tasks to the executor
-            futures = [
-                executor.submit(update_single_sheet, table["id"], table_name)
-                for table_name, table in CONFIG["tables"].items()
-            ]
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Submit all table processing tasks to the executor
+        futures = [
+            executor.submit(update_single_sheet, table["id"], table_name)
+            for table_name, table in Table.config.tables
+        ]
 
-            # Collect results as they complete
-            for x, future in enumerate(concurrent.futures.as_completed(futures)):
-                pass
-                print(f"thread no. {x} returned")
+        # Collect results as they complete
+        for x, _ in enumerate(concurrent.futures.as_completed(futures)):
+            pass
+            print(f"thread no. {x} returned")
 
 
 def update_single_sheet(table_id, table_name):
@@ -275,37 +355,25 @@ def update_single_sheet(table_id, table_name):
 
 def make_summary():
     print("Creating blank summary sheet in folder...")
-    if isinstance(CONFIG, dict):
-        folder_id = CONFIG.get("env", {}).get("target_folder")
-        # ss_api.create_blank_summary_sheet_in_folder(folder_id)
-        print("Blank summary sheet created in folder.")
+    # ss_api.create_blank_summary_sheet_in_folder(folder_id)
+    print("Blank summary sheet created in folder.")
+
+
+def main():
+    Table.config = Config()
+    if Table.config.function == "get":
+        get_sheet()
+    elif Table.config.function == "set":
+        set_sheet()
+    elif Table.config.function == "update":
+        update_sheet()
+    elif Table.config.function == "summary":
+        make_summary()
 
 
 if __name__ == "__main__":
-    with open(_conf, "r") as conf:
-        CONFIG = toml.load(conf)
-        if isinstance(CONFIG, dict):
-            for k, v in CONFIG["env"].items():
-                os.environ[k] = j
-            print(CONFIG.get("verbose"))
-            if CONFIG.get("verbose", False):
-                logging.basicConfig(
-                    filename="sheet.log", filemode="w", level=logging.INFO
-                )
 
-            # Check the command-line argument and call the appropriate function
-            if sys.argv[1] == "get":
-                get_sheet()
-            elif sys.argv[1] == "set":
-                set_sheet()
-            elif sys.argv[1] == "update":
-                update_sheet()
-            elif sys.argv[1] == "summary":
-                make_summary()
-
-    if isinstance(CONFIG, dict):
-        with open(_conf, "w") as conf:
-            toml.dump(CONFIG, conf)
+    main()
 
 
 end_time = datetime.now()
