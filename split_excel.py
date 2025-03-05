a181d1d0e00fd968256ad04696441d861eb88c42
silasbash <silas.bash@oliverwyman.com>
2025-03-05 16:01:58 -0500

    fixed columns

--- split_excel.py
+++ split_excel.py
@@ -2,8 +2,8 @@ import os
 import polars as pl
 
 # Define paths
-input_folder = "in"
-output_folder = "Effectivity_Reports_Split"
+input_folder = "smartsheet"
+output_folder = "smartsheet_split"
 
 # Create the output directory if it doesn't exist
 os.makedirs(output_folder, exist_ok=True)
@@ -23,7 +23,11 @@ for file_name in os.listdir(input_folder):
 
         # Iterate over each group and save to a new Excel file
         for ac_value, group in grouped:
+            # Check if ac_value is a tuple and extract the first element
+            if isinstance(ac_value, tuple):
+                ac_value = ac_value[0]
+
             output_file = os.path.join(output_folder, f"{base_name}_AC{ac_value}.xlsx")
-            group.write_excel(output_file,include_header=False)
+            group.write_excel(output_file, include_header=True)
 
 print("Splitting complete.")
