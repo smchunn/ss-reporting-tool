ff6b7cb9af84c3c5601e224c4dac011f3264b72b
silasbash <silas.bash@oliverwyman.com>
2025-04-24 15:32:44 -0400

    fixed indent typo

--- ss_uploader.py
+++ ss_uploader.py
@@ -235,7 +235,7 @@ class Table:
             and isinstance(rows, pl.Expr)
         ):
 
-            data = [
+                data = [
                 {
                     "toTop": "true",
                     "cells": [
